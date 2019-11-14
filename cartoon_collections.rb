0e020e0ad5b4b5d14fcadffbee28ec2c9be8de1d
Shahwar Spall <shevy.spall@gmail.com>
2019-11-14 07:21:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+ dwarves.each_with_index {|dwarf, index| puts "#{index +1} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(arrays)
+  arrays.map {|array| array.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+   calls_long.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
