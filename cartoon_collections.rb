00511a312f0cc1155a9f7a4e216d4bf240389165
Shahwar Spall <shevy.spall@gmail.com>
2019-11-14 07:23:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 def find_the_cheese(arrays)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arrays.find? {|array| array}
+  arrays.include? {|array| array}
 end
